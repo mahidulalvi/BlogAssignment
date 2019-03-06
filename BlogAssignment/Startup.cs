4c76b39163ec020ca5344b6404957eb3b33f4cef
MD MAHIDUL HAQUE ALVI <mdmahidulhaque001@gmail.com>
2019-03-06 14:31:41 -0600

    Add project files.

---
+++ BlogAssignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogAssignment.Startup))]
+namespace BlogAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
